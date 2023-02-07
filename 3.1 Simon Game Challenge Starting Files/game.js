394e676e28d57849ebcf521c429f863401d31336
shad-abdulsamad <shad.A.A2002@gmail.com>
2023-02-07 21:55:19 +0300

    Add files via upload

---
+++ 3.1 Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,28 @@
+
+let buttonColor=["red", "blue", "green", "yellow"];
+let gamePattern=[];
+let userClickedPattern=[];
+
+$(".btn").click(function(){
+    let userChosenColor=$(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+
+
+});
+function nextSequene(){
+    let randomNumber=Math.floor(Math.random()*4);
+    let randomChosenColor=buttonColor[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+    $("#"+randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    let audio=new Audio("sounds/"+randomChosenColor+".mp3");
+    audio.play();
+}
+/* $("#green").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+$("#red").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+$("#yellow").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+$("#blue").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100); */
+
+
+
